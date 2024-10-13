0b5e21bcdb011b88c9b12b1b22f1cbd98c800965
Alejandro Veintimilla <135124169+VeintimillAlejandro@users.noreply.github.com>
2024-10-13 22:28:26 +0200

    13 - 20
    Completar ejercicio

--- ALUMNOS/MIA/ALEJANDRO_VEINTIMILLA/LINUX/script.sh
+++ ALUMNOS/MIA/ALEJANDRO_VEINTIMILLA/LINUX/script.sh
@@ -52,10 +52,45 @@ echo 12.Mostrar el día y la hora actual.
 
 date
 
-echo 13.Con un solo comando posicionarse en el directorio $HOME.
+echo 13.Con un solo comando posicionarse en el directorio HOME.
 
+cd $HOME
 
+echo 14.Verificar que se está en él.
 
+echo $PATH
 
+echo 15.Listar todos los ficheros del directorio HOME mostrando sus permisos.
 
+ls -l
 
+echo 16.Borrar todos los archivos y directorios visibles de vuestro directorio PRUEBA.
+
+rm -r PRUEBA
+
+echo 17.Crear los directorios dir1, dir2 y dir3 en el directorio PRUEBA. Dentro de dir1 crear el directorio dir11. Dentro del directorio dir3 crear el directorio dir31. Dentro del directorio dir31, crear los directorios dir311 y dir312.
+
+mkdir PRUEBA
+cd PRUEBA
+mkdir dir1 dir2 dir3
+cd dir1
+mkdir dir11
+cd ..
+cd dir3
+mkdir dir31
+cd dir31
+mkdir dir311 dir312
+
+echo 18.Copiar el archivo /etc/mtab a un archivo llamado mensaje de vuestro directorio PRUEBA.
+
+cd ..
+cd ..
+cp /etc/mtab ~/PRUEBA
+mv mtab mensaje 
+cp mensaje ~/PRUEBA/dir1 
+cp mensaje ~/PRUEBA/dir2
+cp mensaje ~/PRUEBA/dir3 
+
+echo 20.Comprobar el ejercicio anterior mediante un solo comando.
+
+ls -lR
