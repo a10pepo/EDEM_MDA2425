9f9bed25184c0b670b7d891e3d58468891bcc871
Coke Stuyck <cokecancook@gmail.com>
2024-10-28 02:17:38 +0100

    Update ahorcado.py
    Rename print words.txt to palabras.txt
    Remove standard alphabet method

--- ALUMNOS/MIA/COKE_STUYCK/AHORCADO/ahorcado.py
+++ ALUMNOS/MIA/COKE_STUYCK/AHORCADO/ahorcado.py
@@ -13,26 +13,6 @@ def read_words(file):
         print(f"Error: File '{file}' not found.")
         sys.exit(1)
 
-def guess_word(word):
-    # Guesses the word by brute-forcing through all the letters of the alphabet.
-    # Returns the number of attempts needed to guess the word.
-
-    guessed_letters = set()  # Tracks the letters that have been correctly guessed
-    attempts = 0
-
-    alphabet = string.ascii_lowercase
-    
-    for letter in alphabet:  # Iterates through the alphabet in order
-        attempts += 1
-        if letter in word.lower():
-            guessed_letters.add(letter)
-        
-        # Checks if all letters in the word have been guessed
-        if all(letter.lower() in guessed_letters for letter in word):
-            return attempts
-    
-    return attempts  # Ensures the function always returns attempts
-
 def guess_word_spanish(word):
     # Guesses the word by brute-forcing through an optimized spanish alphabet.
     # Returns the number of attempts needed to guess the word.
@@ -56,17 +36,16 @@ def guess_word_spanish(word):
 def main():
     # Checks if the file has been provided as an argument
     if len(sys.argv) != 2:
-        print("Usage: python3 ahorcado.py words.txt")
+        print("Usage: python3 ahorcado.py palabras.txt")
         sys.exit(1)
     
     words_file = sys.argv[1]
     words = read_words(words_file)
-
+    print("\nWith the optimized spanish alphabet:\n")
     for word in words:
-        attempts = guess_word(word)
-        print(f"\nWord: {word} - Attempts needed: {attempts}")
         attempts_optimized = guess_word_spanish(word)
-        print(f"Word: {word} - Attempts needed: {attempts_optimized} (optimized spanish alphabet)")
-
+        print(f"Word: {word} - Attempts needed: {attempts_optimized}")
+    print("")
+    
 if __name__ == "__main__":
     main()
