7286c7404981662f631c176dc4f1ce4813fdcb9a
fab1an12 <fabianmiulescu@gmail.com>
2024-10-29 19:09:06 +0100

    Fase almacenaje
    Creacion del docker-compose y la base de datos

--- ALUMNOS/MDAB/FABIAN_MIULESCU/AHORCADO/ahorcado.py
+++ ALUMNOS/MDAB/FABIAN_MIULESCU/AHORCADO/ahorcado.py
@@ -1,7 +1,49 @@
 import sys
+import os
+import mysql.connector
+from datetime import datetime
+
+db_config = {
+    'user': os.getenv('DB_USER', 'user'),
+    'password': os.getenv('DB_PASSWORD', 'password'),
+    'host': os.getenv('DB_HOST', 'mysql'),
+    'database': os.getenv('DB_NAME', 'ahorcado_db')
+}
+
+def conectar_db():
+    return mysql.connector.connect(**db_config)
+
+def crear_tabla():
+    conn = conectar_db()
+    with conn.cursor() as micursor:
+        
+        micursor.execute("DROP TABLE IF EXISTS intentos_ahorcado")
+        micursor.execute("""
+            CREATE TABLE IF NOT EXISTS intentos_ahorcado (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                palabra VARCHAR(255),
+                letras_acertadas TEXT,
+                letras_falladas TEXT,
+                intentos INT,
+                tiempo TIMESTAMP
+            )
+        """)
+        conn.commit()
+    conn.close()
+
+def registrar_intento(palabra, letras_acertadas, letras_falladas, intentos):
+    conn = conectar_db()
+    with conn.cursor() as micursor:
+        tiempo_actual = datetime.now()
+        micursor.execute("""
+            INSERT INTO intentos_ahorcado (palabra, letras_acertadas, letras_falladas, intentos, tiempo)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (palabra, letras_acertadas, letras_falladas, intentos, tiempo_actual))
+        conn.commit()
+    conn.close()
 
 def adivinar_palabras(archivo):
-    abecedario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    abecedario = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
     total_intentos = 0
     
     with open(archivo, 'r') as f:
@@ -25,12 +67,14 @@ def adivinar_palabras(archivo):
                 else:
                     letras_bien += letra
                 intentos += 1
+                registrar_intento(palabra, letras_bien, letras_mal, intentos)
 
             total_intentos += intentos    
             print(f"\nPalabra '{palabra}' adivinada en {intentos} intentos.")
             print(f"Letras bien {letras_bien}")
             print(f"Letras bien {letras_mal}")
     print(f"\nTotal de intentos utilizados en todas las palabras: {total_intentos}")
-    
+
+crear_tabla()    
 file = sys.argv[1]
 adivinar_palabras(file)
