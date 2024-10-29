c56cc190628c692dda5482cba972c088a4b81b64
fab1an12 <fabianmiulescu@gmail.com>
2024-10-29 16:29:10 +0100

    Fase de codificacion
    Metodo de adivinar palabras y fichero de palabras

---
+++ ALUMNOS/MDAB/FABIAN_MIULESCU/AHORCADO/ahorcado.py
@@ -0,0 +1,36 @@
+import sys
+
+def adivinar_palabras(archivo):
+    abecedario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    total_intentos = 0
+    
+    with open(archivo, 'r') as f:
+        for palabra in f:
+            palabra = palabra.strip().upper()
+            palabra_actual = ['X'] * len(palabra)
+            letras_bien = ""
+            letras_mal = ""
+            intentos = 0
+
+            for letra in abecedario:
+                if 'X' not in palabra_actual:
+                    break
+                fallo = True
+                for i, caracter in enumerate(palabra):
+                    if caracter == letra:
+                        palabra_actual[i] = letra
+                        fallo = False
+                if fallo:
+                    letras_mal += letra
+                else:
+                    letras_bien += letra
+                intentos += 1
+
+            total_intentos += intentos    
+            print(f"\nPalabra '{palabra}' adivinada en {intentos} intentos.")
+            print(f"Letras bien {letras_bien}")
+            print(f"Letras bien {letras_mal}")
+    print(f"\nTotal de intentos utilizados en todas las palabras: {total_intentos}")
+    
+file = sys.argv[1]
+adivinar_palabras(file)
