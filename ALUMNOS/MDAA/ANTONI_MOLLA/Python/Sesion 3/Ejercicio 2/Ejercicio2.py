cdecebd545c6d24583945fe348d270bfb904dc44
AntoniBI <mollantoni02@gmail.com>
2024-11-06 16:34:42 +0100

    Update

--- ALUMNOS/MDAA/ANTONI_MOLLA/Python/Sesion 3/Ejercicio 2/Ejercicio2.py
+++ ALUMNOS/MDAA/ANTONI_MOLLA/Python/Sesion 3/Ejercicio 2/Ejercicio2.py
@@ -2,7 +2,10 @@
 
 for num in range (1,101): #Range=Rango
     if num <= 1:
-        continue #Sigue iterando sobre los valores menores de num para verificar si es numero primo
+
+#Sigue iterando sobre los valores menores de num para verificar si es numero primo
+        continue
+    
 #Continue da la orden de seguir on el orden del codigo
     for i in range(2, num):
         if num % i ==0:
--- ALUMNOS/MDAA/ANTONI_MOLLA/Python/Sesion 3/Ejercicio3/Ejercicio3.py
+++ ALUMNOS/MDAA/ANTONI_MOLLA/Python/Sesion 3/Ejercicio3/Ejercicio3.py
@@ -1,9 +1,12 @@
 
-lista_Años= [1820,1950, 1930, 1955, 1980,1992, 2000, 2010, 2024, 2050, 2100]
+lista_Años= [1820,1950,1930,1955,1980,1992,2000,2010,2024,2050,2100]
 
-
+#Realizamos un bucle que itere sobre los diversos años
 for años in lista_Años:
+
+#Realizamos una condicion para conocer si un año es bisiesto o no
     if( años % 4 == 0 and años % 100 != 0) or (años % 400 == 0):
+
         print(f'El año {años} es bisiesto')
     
     else:
