d7980e0c5734994533b72e0d9abca82e13d89414
martamiro <martamiro0@gmail.com>
2024-11-08 13:38:31 +0100

    Sesion 2 python Marta Miró

--- ALUMNOS/MDAA/MARTA_MIRO/PYTHON/Sesion2.py
+++ ALUMNOS/MDAA/MARTA_MIRO/PYTHON/Sesion2.py
@@ -13,4 +13,4 @@ print("Has indicado que la inversión durará:", años_inversion, "años")
 #4
 resultado_final = cantidad_inversion * (1 + interes_decimal) ** años_inversion
 interes_generado = resultado_final - cantidad_inversion
-print((f"En {años_inversion} años habrás recibido {interes_generado}€ de interés."))
+print((f"En {años_inversion} años habrás recibido {interes_generado}€ de interés.")) 
