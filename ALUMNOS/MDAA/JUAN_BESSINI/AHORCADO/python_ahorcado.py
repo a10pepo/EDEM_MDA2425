213ae3365ba9875db4a986e4810a9a94b879c427
Juan BESSINI <juangabrielbessini@gmail.com>
2024-11-22 20:32:38 +0100

    Cierre de clase 20.30

--- ALUMNOS/MDAA/JUAN_BESSINI/AHORCADO/python_ahorcado.py
+++ ALUMNOS/MDAA/JUAN_BESSINI/AHORCADO/python_ahorcado.py
@@ -1,5 +1,12 @@
 #AHORCADO GAME made by JBESSINI
 
+import sys
+import pg8000.native
+con = pg8000.native.Connection("postgres", password="Welcome01")
+
+# Crear una tabla temporal
+con.run("CREATE TEMPORARY TABLE Tabla_ahorcado (palabra VARCHAR, letras_acertadas VARCHAR, letras_falladas VARCHAR, intentos VARCHAR, tiempo DATE)")
+
 # Abecedario como lista de letras
 abecedario = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
                "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -9,14 +16,15 @@ abecedario_opt = ["E", "A", "O", "S", "R", "N", "I", "D", "L", "C", "T",
                          "J", "Ñ", "X", "K", "W"]
 
 # Código para leer un archivo y generar una lista con las líneas
-filename = "C:\\Users\\jbessini\\Documents\\GitHub\\EDEM_MDA2425\\ALUMNOS\\MDAA\\JUAN_BESSINI\\AHORCADO\\palabras.txt"
+# filename = "C:\\Users\\jbessini\\Documents\\GitHub\\EDEM_MDA2425\\ALUMNOS\\MDAA\\JUAN_BESSINI\\AHORCADO\\palabras.txt"
+filename = sys.argv[1]
 
 # Leer el archivo y crear la lista
 with open(filename, "r") as file:
     palabras = [line.strip() for line in file]
 
 # Imprimir la lista resultante
-print(palabras)
+# print(palabras)
 
 # Empieza el juego
 intentos = 0
@@ -29,7 +37,7 @@ for palabra in palabras:
             aciertos +=1
         else:
             fallos +=1
-print(intentos)
+# print(intentos)
 # print(aciertos)
 # print(fallos)
 
@@ -58,3 +66,15 @@ for palabra in palabras:
 print(intentos)
 # print(letras_encontradas)
 # print(letras_unicas)
+
+# Rellenar la tabla
+for palabra in ("Ender's Game", "The Magus"):
+    con.run("INSERT INTO Tabla_ahorcado (palabra) VALUES (:palabra)", title=title)
+
+# Imprime los valores
+for row in con.run("SELECT * FROM book"):
+     print(row)
+[1, "Ender's Game"]
+[2, 'The Magus']
+
+con.close()
