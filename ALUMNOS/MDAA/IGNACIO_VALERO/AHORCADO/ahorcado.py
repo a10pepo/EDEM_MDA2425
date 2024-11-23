0f6bb85a384fe11630b63ace366725d9f3012b04
IgnaValero <Ignavalero@gmail.com>
2024-11-23 10:53:09 +0100

    Ahorcado

--- ALUMNOS/MDAA/IGNACIO_VALERO/AHORCADO/ahorcado.py
+++ ALUMNOS/MDAA/IGNACIO_VALERO/AHORCADO/ahorcado.py
@@ -1,7 +1,7 @@
 DICCIONARIO = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
               'N','Ñ','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-import pg8000
-con = pg8000.native.Connection("postgres", password="Welcome01")
+import pg8000.native
+con = pg8000.native.Connection("postgres", password="Welcome01",host="postgres_container")
 
 import sys
 def leer_archivo():
@@ -16,21 +16,22 @@ def leer_archivo():
 palabras = leer_archivo()
 print(palabras)
 intentos = 0
-letras_acertadas =0
-letras_falladas= 0
+
 
 for palabra in palabras:
+    letras_acertadas = ""
+    letras_falladas= ""
     longitud = len(palabra)
     for letra in DICCIONARIO:
         if longitud >0:
             intentos += 1
-            con.run("INSERT INTO book (ahorcado) VALUES (:ahorcado)", title=title)
+            con.run("INSERT INTO ahorcado (palabra,letras_acertadas,letras_falladas) VALUES (:palabra,:letras_acertadas,:letras_falladas)", palabra=palabra,letras_acertadas=letras_acertadas,letras_falladas=letras_falladas)
             if letra in palabra:
                 longitud= longitud - 1 * palabra.count(letra)
                 print(longitud)
-                #letras_acertadas += letra
-            #else:
-                #letras_falladas += letra
+                letras_acertadas += letra
+            else:
+                letras_falladas += letra
     
 
 print(intentos)
