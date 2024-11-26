57080f904a82b05ad74eaa1b156ac836bbc205e5
e-wach <e_wangting@hotmail.com>
2024-11-26 16:44:39 +0100

    Update ahorcado.py

--- ALUMNOS/MDAB/TING_WANG/AHORCADO/ahorcado.py
+++ ALUMNOS/MDAB/TING_WANG/AHORCADO/ahorcado.py
@@ -1,31 +1,62 @@
-# Importar abecedario
 import string
-letras = string.ascii_uppercase
+import psycopg2
+from datetime import datetime
 
+letras = string.ascii_uppercase
 abecedario = list(letras)
 
-
-# Vincular el archivo txt al py.
 with open('palabras.txt','r') as fichero:
      palabras = fichero.read().splitlines()
 
 
-intentos = 0
-aciertos = 0
+conn_target = psycopg2.connect(
+    dbname="AHORCADO",
+    user="postgres",
+    password="Welcome01",
+    host="localhost",
+    port="5432" 
+)
+cursor = conn_target.cursor()
+
+crear_tabla = """
+    CREATE TABLE IF NOT EXISTS ahorcado_palabras (
+        palabra TEXT,
+        letras_acertadas TEXT,
+        letras_falladas TEXT,
+        intentos INT,
+        tiempo TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+"""
+cursor.execute(crear_tabla)
+conn_target.commit()
+
+
 for palabra in palabras:
-     for letra in abecedario:
+    intentos = 0
+    aciertos = 0
+    letras_acertadas = []
+    letras_falladas = []
+    for letra in abecedario:
         intentos = intentos + 1
-        if letra in palabra:
+        if letra in palabra:           
             print(letra + " en " + palabra)
             aciertos = aciertos + palabra.count(letra)
+            letras_acertadas.append(letra)
             if aciertos == len(palabra):
                 print('Acerté la palabra')
                 aciertos = 0
                 break
-            # else:
-            #     print(aciertos)
-            #     print(len(palabra))
-            #     print(intentos)
+            else:
+                letras_falladas.append(letra)
 
+        insertar =  """
+            INSERT INTO ahorcado_palabras (palabra, letras_acertadas, letras_falladas, intentos, tiempo)
+            VALUES (%s, %s,%s, %s,%s)
+            """
+        cursor.execute(insertar, (palabra, ''.join(letras_acertadas), ''.join(letras_falladas), intentos, datetime.now()))
 
-print(intentos)
+conn_target.commit()
+cursor.close()
+conn_target.close()
+
+print("Datos insertados correctamente.")
