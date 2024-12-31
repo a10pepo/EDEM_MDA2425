ff109d52d973fdd50cafbbd2610e500aed52cbe4
Joel Seguí Far <joelsf43@gmail.com>
2024-12-31 13:17:33 +0100

    Paso 1

---
+++ ALUMNOS/MDAB/JOEL_SEGUI/CHATBIG/consumer.py
@@ -0,0 +1,41 @@
+from confluent_kafka import Consumer, KafkaError
+
+# Configuración del consumidor
+def create_consumer():
+    config = {
+        'bootstrap.servers': 'localhost:9092', 
+        'group.id': 'python-consumer-group', 
+        'auto.offset.reset': 'earliest'   
+    }
+    return Consumer(config)
+
+# Crear un consumidor
+consumer = create_consumer()
+
+# Suscribirse a un tópico
+topic = 'censura'  # El nombre del tópico
+consumer.subscribe([topic])
+
+# Loop infinito de consumo de mensajes del topic
+try:
+    while True:
+        msg_consumer = consumer.poll(1.0)  # Lee nuevos mensajes cada 1 segundo
+        
+        if msg_consumer is None: #si no hay mensage, siguiente (vuelve a empezar el while)
+            continue
+        if msg_consumer.error():
+            if msg_consumer.error().code() == KafkaError._PARTITION_EOF:
+                print("No hay más mensajes en esta partición.")
+            else:
+                print("Error al recibir mensaje: {}".format(msg_consumer.error()))
+        else:
+            # Procesar el mensaje recibido. Aqui solo lo mostramos por pantall. En una App real se pueden hacer cualquier
+            # cosa con un mensaje. Ejmplos: Filtrarlo, modificarlo, guardarlo en una base de datos, 
+            #enviarlo a otra applicación, etc.
+            print('Nuevo mensaje: {}'.format(msg_consumer.value().decode('utf-8')))
+
+except KeyboardInterrupt:
+    pass
+finally:
+    # Cerrar el consumidor al parar la Applicacion Python
+    consumer.close()
