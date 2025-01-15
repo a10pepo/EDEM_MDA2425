a9dc2004220b7132109ffd103397738c967ed141
vicentmg6 <99805687+vicentmg6@users.noreply.github.com>
2025-01-15 20:07:44 +0100

    Revert "Merge"
    This reverts commit 95fa7df9be69e312ed1dd5d3e604ddf9046df38e.

--- ALUMNOS/MIA/ALEJANDRO_VEINTIMILLA/PYTHON/Curso de Python/Sesión 1/Ejercicio 1/main.py
+++
@@ -1,4 +0,0 @@
-#1. Ejercicios Sesión 1
-#   1.Crea un archivo Python que muestre por consola "¡Hola, Mundo!"
-
-print("¡Hola, Mundo!")
--- ALUMNOS/MIA/ALEJANDRO_VEINTIMILLA/PYTHON/Curso de Python/Sesión 5/Ejercicio 1/main.py
+++
@@ -1,2 +0,0 @@
-#5. Ejercicios Sesión 5
-#   1.Lee un archivo CSV con Pandas y realizar las siguientes operaciones:
