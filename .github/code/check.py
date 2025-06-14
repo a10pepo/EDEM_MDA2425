30e873042afea051a74fc5cc3b0136573ab38c38
Pedro Nieto <pedro.nieto@onlyfy.com>
2025-06-14 16:45:36 +0200

    adding MDA_CLOUD

--- .github/code/check.py
+++ .github/code/check.py
@@ -5,19 +5,26 @@ import traceback
 comun_obligatorio=["DOCKER","PYTHON","SQL","AHORCADO","LINUX"]
 mia1_obligatorio=["ESTADISTICA"]
 mda1_obligatorio=["APIS", "CHUCK", "DBT", "KAFKA", "PYSPARK"]
+mda2_obligatorio=["AWS_ALMACENAMIENTO", "GCP_ALMACENAMIENTO"]
+
 
 
 def check_class(folder_path):
     class_type=folder_path.split("/")[-1][0:3]
     deliverables=os.listdir(os.path.join(os.getcwd(), "PROFESORES"+"/"+class_type))
     deliverables=deliverables+os.listdir(os.path.join(os.getcwd(), "PROFESORES"+"/COMUN"))
+    if ".DS_Store" in deliverables:
+        deliverables.remove(".DS_Store")
     alumnos={}
     
     for alumno in os.listdir(folder_path):
         file_path = os.path.join(folder_path, alumno)
+        if alumno.startswith('ANTONI'):
+            print("TEST")
         comunes=0
         mia1=0
         mda1=0
+        mda2=0
         if os.path.isdir(file_path):
             delivs={}
             alumnos[alumno]=delivs
@@ -34,12 +41,16 @@ def check_class(folder_path):
                     if "MDA" in class_type:
                         if element in mda1_obligatorio:
                             mda1+=1
+                        if element in mda2_obligatorio:
+                            mda2+=1                                                       
                 else:
                     print("Entregable "+element+" NO Existe para el alumno "+alumno)
                     alumnos[alumno][element]=False
         alumnos[alumno]["NOTA COMUNES"]=comunes*10/len(comun_obligatorio)
         if "MDA" in class_type:
             alumnos[alumno]["MDA_M1"]=mda1*10/len(mda1_obligatorio)
+            alumnos[alumno]["MDA_M2"]=mda2*10/len(mda2_obligatorio)
+            
         if "MIA" in class_type:
             alumnos[alumno]["MIA_M1"]=mia1*10/len(mia1_obligatorio)
     return alumnos
@@ -59,15 +70,17 @@ def generate_table(clase,alumnos):
     deliverables=os.listdir(os.path.join(os.getcwd(), "PROFESORES"+"/"+class_type))
     deliverables=deliverables+os.listdir(os.path.join(os.getcwd(), "PROFESORES"+"/COMUN"))
     deliverables=deliverables+["NOTA COMUNES"]
+    if ".DS_Store" in deliverables:
+        deliverables.remove(".DS_Store")
     if "MDA" in class_type:
-        deliverables=deliverables+["MDA_M1"]
+        deliverables=deliverables+["MDA_M1","MDA_M2"]
     if "MIA" in class_type:
         deliverables=deliverables+["MIA_M1"]
     print("Generating Table")
     try:
         table="<table>\n<tr><th>Alumno</th>"
-        for element in deliverables:
-            if element in mda1_obligatorio+comun_obligatorio+mia1_obligatorio:
+        for element in deliverables:     
+            if element in mda1_obligatorio+comun_obligatorio+mia1_obligatorio+mda2_obligatorio:
                 table+="\n<th>*"+element+"*</th>"
             else:
                 table+="\n<th>"+element+"</th>"
@@ -77,12 +90,12 @@ def generate_table(clase,alumnos):
             table+="<tr>\n<td><a href='https://github.com/a10pepo/EDEM_MDA2425/tree/main/ALUMNOS/"+clase+"/"+alumno+"'>"+str.upper(alumno)+"</a></td>"
             for element in deliverables:
                 if alumnos[alumno][element]:
-                    if element in ("NOTA COMUNES","MDA_M1","MIA_M1"):
+                    if element in ("NOTA COMUNES","MDA_M1","MIA_M1","MDA_M2"):
                         table+="\n<td>"+str(alumnos[alumno][element])+"</td>"
                     else:
                         table+="\n<td>✅</td>"
                 else:
-                    if element in ("NOTA COMUNES","MDA_M1","MIA_M1"):
+                    if element in ("NOTA COMUNES","MDA_M1","MIA_M1","MDA_M2"):
                         table+="\n<td>0.0</td>"
                     else:
                         table+="\n<td>❌</td>"
@@ -134,4 +147,3 @@ if __name__ == '__main__':
     check_names(os.path.join(os.getcwd(), "ALUMNOS/MIA"))
     modify_readme()    
     print("README.md updated")
-
